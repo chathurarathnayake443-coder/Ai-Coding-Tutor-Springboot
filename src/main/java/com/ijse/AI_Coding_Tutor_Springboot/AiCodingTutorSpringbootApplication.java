3e8c3c6c69cfb097e39d76ede6c3d2c8f889a3f5
Chathura Rathanayake <chathurarathnayake443@gmail.com>
2026-08-09 18:13:24 +0530

    first commit

---
+++ src/main/java/com/ijse/AI_Coding_Tutor_Springboot/AiCodingTutorSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.ijse.AI_Coding_Tutor_Springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCodingTutorSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AiCodingTutorSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ijse/AI_Coding_Tutor_Springboot/AiCodingTutorSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ijse.AI_Coding_Tutor_Springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCodingTutorSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
